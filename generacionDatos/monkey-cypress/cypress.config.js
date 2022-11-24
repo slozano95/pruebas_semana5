a70123032affa817783e258e5eb16771c98fcc4c
Santiago Lozano <santiago@slr.local>
2022-11-24 22:38:07 +0100

    Fixed cypress config file

--- generacionDatos/monkey-cypress/cypress.config.js
+++ generacionDatos/monkey-cypress/cypress.config.js
@@ -3,16 +3,7 @@ module.exports = {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      on('task', {
-        readFileMaybe(filename) {
-          if (fs.existsSync(filename)) {
-            return fs.readFileSync(filename, 'utf8')
-          }
-      
-          return null
-        }
-      })
-      // return require('./cypress/plugins/index.js')(on, config);
+      return require('./cypress/plugins/index.js')(on, config);
     }
 
   }

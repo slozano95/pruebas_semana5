b004afea88430a9c2f5391f6f0587d373a8920aa
Santiago Lozano <santiago@slr.local>
2022-11-25 19:54:01 +0100

    Added apriori cases and mocks

---
+++ generacionDatos/monkey-cypress/cypress/e2e/monkey/45_apriori_create_page_custom_url_special_chars.cy.js
@@ -0,0 +1,51 @@
+import { DataPool, PoolOrigin } from "./_datapool";
+import {login, waitSeconds, clickOnButton, clickOnRawButton, clickOnLink} from './_shared_slr';
+require('cypress-plugin-tab');
+
+const url = Cypress.config('baseUrl') || "http://localhost:2368/ghost/"
+const username = Cypress.config('username') || "slozano95@gmail.com";
+const pwd = Cypress.config('password') || "hola123456";
+
+Cypress.on('uncaught:exception', (err)=>{
+    cy.task('logCommand', {'message':`An exception occurred: ${err.message}`})
+    cy.task('genericReport', {'html': `<p><strong>Uncaught exception: </strong>${err}</p>`});
+    return false
+});
+
+describe(`Ghost is under smarter monkeys`, function() {
+    beforeEach(() => {
+        cy.readFile('./mocks/45.json').then((fileData) => {
+            DataPool.prepare(PoolOrigin.APriori, fileData);
+        })
+    });
+    
+    it('Creates page with custom url and special chars', function() { 
+        cy.visit(url).then((win)=>{  
+            login(username, pwd);
+            waitSeconds(1);
+            clickOnLink("Pages");
+            clickOnLink("New page");
+            waitSeconds(1);
+            cy.focused().type(DataPool.get("title"))
+            waitSeconds(1);
+            cy.focused().type("{enter}")
+            waitSeconds(1);
+            cy.focused().type(DataPool.get("body"))
+            cy.get(`.settings-menu-toggle`).click({force:true});
+            waitSeconds(1);
+            cy.get('.post-setting-slug').clear({force: true});
+            cy.get('.post-setting-slug').type(DataPool.get("url"), {force: true});
+            clickOnButton("Publish");
+            clickOnButton("Continue");
+            clickOnButton("Publish page");
+            waitSeconds(1);
+            cy.contains("Error")
+        })
+        cy.wait(1000)
+    })
+   
+    afterEach(()=>{
+        cy.log('logEnd')
+        return false;
+    })
+})
